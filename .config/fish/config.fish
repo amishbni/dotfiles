2df4e2563cd167f6e8cb079e74dbfb43d0737645
Amir Shabani <a.shabani@bluteam.ir>
2025-09-05 19:31:29 +0330

    add cursor, pycharm, and webstorm aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,7 @@ if status is-interactive
 	set -U fish_greeting ""
 	starship init fish | source
 	alias idea "setsid intellij-idea-community; sleep 3; exit"
+	alias cursor "setsid $HOME/Applications/cursor/Cursor-1.2.1-x86_64.AppImage --no-sandbox; sleep 3; exit"
+	alias pycharm "setsid $HOME/Applications/pycharm/bin/pycharm; sleep 3; exit"
+	alias webstorm "setsid $HOME/Applications/webstorm/bin/webstorm; sleep 3; exit"
 end
